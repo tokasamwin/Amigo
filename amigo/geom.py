51c15083372cd37b9d4eafa0678f31516026de92
twistersi <twistersi@gmail.com>
2016-09-08 07:12:55 +0200

    minor update

--- amigo/geom.py
+++ amigo/geom.py
@@ -86,7 +86,7 @@ def space(R,Z,npoints):
     
 def rotate(theta):
     Rz = np.array([[np.cos(theta),-np.sin(theta),0],
-                   [np.sin(theta),-np.cos(theta),0],
+                   [np.sin(theta),np.cos(theta),0],
                    [0,0,1]])
     return Rz
     
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='amigo',
-      version='0.0.1',
+      version='0.0.2',
       description='equlibrium tools',
       url='https://github.com/twistersi/Amigo',
       author='Simon McIntosh',
